9a2bc7052db1c5af5ecbcac5f2e17df749588fef
jealdana <aldana.sigona@gmail.com>
2020-01-07 15:35:05 +0200

    workout tracking

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import Chart from "react-google-charts";
 
 function App() {
+  var [wo_calendar, setWoCalendar] = useState([]);
+
+  var array_data = [
+    [new Date(2012, 3, 13), 37032],
+    [new Date(2012, 3, 14), 38024],
+    [new Date(2012, 3, 15), 38024],
+    [new Date(2012, 3, 16), 38108],
+    [new Date(2012, 3, 17), 38229],
+    [new Date(2013, 1, 4), 38177],
+    [new Date(2013, 1, 5), 38705],
+    [new Date(2013, 1, 12), 38210],
+    [new Date(2013, 1, 13), 38029],
+    [new Date(2013, 1, 19), 38823],
+    [new Date(2013, 1, 23), 38345],
+    [new Date(2013, 1, 24), 38436],
+    [new Date(2013, 1, 24), 38436]
+  ];
+
+  var testData = [
+    [
+      { type: "date", id: "Date" },
+      { type: "number", id: "Won/Loss" }
+    ],
+    ...array_data
+  ];
+
+  const [items, setItems] = useState([]);
+
+  const addItem = () => {
+    setItems([
+      ...items,
+      {
+        id: items.length,
+        value: new Date().toJSON().slice(0, 10)
+      }
+    ]);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Chart
+        width={1000}
+        height={400}
+        chartType="Calendar"
+        loader={<div>Loading Chart</div>}
+        data={testData}
+        options={{
+          title: "Wo"
+        }}
+        rootProps={{ "data-testid": "1" }}
+      />
+
+      <button onClick={addItem}>Add a number</button>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.value}</li>
+        ))}
+      </ul>
     </div>
   );
 }
